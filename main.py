83deecbcbbd21ae1c2839d48a92688b1e787b726
BrettB <brettbedarf.dev@gmail.com>
2020-12-31 10:50:25 -0700

    load epic availability calendar

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import sys
+
 from selenium import webdriver
 import selenium.common.exceptions as Exceptions
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+
 import config.epic_config as epic_config
 
 
@@ -9,6 +16,7 @@ def main():
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get("https://www.epicpass.com/plan-your-trip/lift-access/reservations.aspx")
 
+    # TODO Sometimes any page load will give system cannot process your request error. Need to check for error screen on ever action
     # Enter credentials and sign in if form is present
     try:
         if driver.find_element_by_id('accountSignIn'):
@@ -25,6 +33,20 @@ def main():
     # NoSuchElementException thrown if sign in form not present
     except Exceptions.NoSuchElementException:
         print('Already logged in!')
+
+    # Select resort from dropdown
+    try:
+        resort_selector = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located(
+                (By.ID,
+                 'PassHolderReservationComponent_Resort_Selection')))
+        Select(resort_selector).select_by_visible_text(
+            epic_config.resort)
+    except Exceptions.NoSuchElementException:
+        sys.exit(f'\nError finding a resort named "{epic_config.resort}". Check the spelling and try again.')
+
+    # Check availability/load calendar
+    driver.find_element_by_id('passHolderReservationsSearchButton').click()
 
     input('Any key to exit...')
 
