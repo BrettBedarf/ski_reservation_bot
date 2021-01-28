d0b175cfd027b91131503f4a3c7bacc1a87a0f65
BrettB <brettbedarf.dev@gmail.com>
2021-01-27 21:20:55 -0700

    Run reservation process from controller after successful form validation.

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, request
 from flask.templating import render_template_string
 
 from app.forms import ReservationForm
+from package.reservation_factory import ReservationFactory
 
 # a simple page that says hello
 @app.route("/", methods=["GET", "POST"])
@@ -10,5 +11,8 @@ from app.forms import ReservationForm
 def index():
     form = ReservationForm()
     if form.validate_on_submit():
+        provider = ReservationFactory()
+        reservation = provider.make_reservation(form.data)
+        reservation.process()
         return render_template_string("Success!")
     return render_template("reservation.html", form=form)
