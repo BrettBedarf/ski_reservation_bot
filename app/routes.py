72bba4f932db8d2f1b907fe52d6fa9a76baceb9e
BrettB <brettbedarf.dev@gmail.com>
2021-01-27 19:23:46 -0700

    Form validation error and success displays

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
 from flask import current_app as app
-from flask import render_template
+from flask import render_template, request
+from flask.templating import render_template_string
 
 from app.forms import ReservationForm
 
 # a simple page that says hello
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 @app.route("/index")
 def index():
     form = ReservationForm()
+    if form.validate_on_submit():
+        return render_template_string("Success!")
     return render_template("reservation.html", form=form)
